baf8c6c8a90e8e46d957ae938075529623538a9f
diano.faniry@gmail.com <diano.faniry@gmail.com>
2023-10-11 13:51:04 +0400

    Version finale

--- src/components/forms/InputRangePrice.jsx
+++ src/components/forms/InputRangePrice.jsx
@@ -3,7 +3,8 @@ export function InputRangePrice({value, onChange})
 {
     return (
         <div>
-            <br />
+            <br/>
+            <label ><strong>Filter by Price :</strong> {value}$ </label>
             <input className="form-range" type="range"  min={0} max={5} value={value} onChange= {(e) => onChange(e.target.value)} />
         </div>
     )
